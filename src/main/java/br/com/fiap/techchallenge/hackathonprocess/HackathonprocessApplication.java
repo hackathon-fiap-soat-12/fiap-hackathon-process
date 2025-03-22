8a988767b9b67366c25cf085e8089f49b24283e0
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-22 19:34:16 -0300

    ✨ feat: Add Spring

---
+++ src/main/java/br/com/fiap/techchallenge/hackathonprocess/HackathonprocessApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.hackathonprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonprocessApplication.class, args);
+	}
+
+}
