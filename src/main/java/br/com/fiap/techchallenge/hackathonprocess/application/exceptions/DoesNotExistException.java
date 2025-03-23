25e5c06c493e67734b45c9baaac4fb0c85e0e8c2
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-23 20:00:05 -0300

    ✨ feat: Create Exceptions

---
+++ src/main/java/br/com/fiap/techchallenge/hackathonprocess/application/exceptions/DoesNotExistException.java
@@ -0,0 +1,9 @@
+package br.com.fiap.techchallenge.hackathonprocess.application.exceptions;
+
+public class DoesNotExistException extends ProcessException {
+
+	public DoesNotExistException(String message) {
+		super(message);
+	}
+
+}
