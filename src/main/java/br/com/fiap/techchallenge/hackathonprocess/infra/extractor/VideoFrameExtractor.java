5f21f1aaf8634e8ae60d03af3667d6b3bbc0dd9b
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-23 20:28:17 -0300

    🐛 fix: remove complexity in VideoFrameExtractor

--- src/main/java/br/com/fiap/techchallenge/hackathonprocess/infra/extractor/VideoFrameExtractor.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonprocess/infra/extractor/VideoFrameExtractor.java
@@ -3,6 +3,7 @@ package br.com.fiap.techchallenge.hackathonprocess.infra.extractor;
 import br.com.fiap.techchallenge.hackathonprocess.application.exceptions.ErrorOnExtractFramesException;
 import br.com.fiap.techchallenge.hackathonprocess.application.service.FrameExtractor;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.springframework.stereotype.Component;
 
@@ -22,24 +23,32 @@ public class VideoFrameExtractor implements FrameExtractor {
             int intervalFrames = frameRate * 20;
 
             try (Java2DFrameConverter converter = new Java2DFrameConverter()) {
-                for (int i = 0; i < grabber.getLengthInFrames(); i++) {
-                    var frame = grabber.grabImage();
-                    if (frame == null) continue;
-
-                    if (i % intervalFrames == 0) {
-                        var bufferedImage = converter.convert(frame);
-                        if (bufferedImage != null) {
-                            var byteArrayOutputStream = new ByteArrayOutputStream();
-                            ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
-                            frameStreams.add(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
-                        }
-                    }
-                }
+                this.extractAndConvert(grabber, intervalFrames, converter, frameStreams);
             }
             grabber.stop();
         } catch (IOException e) {
             throw new ErrorOnExtractFramesException("Error On Extract Frames");
         }
         return frameStreams;
+    }
+
+    private void extractAndConvert(FFmpegFrameGrabber grabber, int intervalFrames, Java2DFrameConverter converter, List<InputStream> frameStreams) throws IOException {
+        for (int i = 0; i < grabber.getLengthInFrames(); i++) {
+            var frame = grabber.grabImage();
+            if (frame == null) continue;
+
+            this.bufferAndWrite(intervalFrames, converter, frameStreams, i, frame);
+        }
+    }
+
+    private void bufferAndWrite(int intervalFrames, Java2DFrameConverter converter, List<InputStream> frameStreams, int i, Frame frame) throws IOException {
+        if (i % intervalFrames == 0) {
+            var bufferedImage = converter.convert(frame);
+            if (bufferedImage != null) {
+                var byteArrayOutputStream = new ByteArrayOutputStream();
+                ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
+                frameStreams.add(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
+            }
+        }
     }
 }
