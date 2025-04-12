84adb083d35161cce263fed038292a704ad74f33
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-04-12 02:51:35 -0300

    ✨ feat: log

--- src/main/java/br/com/fiap/techchallenge/hackathonprocess/infra/config/filestorage/AwsS3Config.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonprocess/infra/config/filestorage/AwsS3Config.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.*;
@@ -17,29 +17,30 @@ public class AwsS3Config {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AwsS3Config.class);
 
-
     @Value("${aws.access-key-id}")
     private String accessKeyId;
 
     @Value("${aws.secret-access-key}")
     private String secretAccessKey;
 
+    @Value("${aws.session-token}")
+    private String sessionToken;
+
     @Bean
     public S3Client s3Client() {
+        LOGGER.info("Initializing S3Client with session credentials");
+        LOGGER.info("Initializing S3Client accessKeyId {} - secretAccessKey {} - sessionToken {}", accessKeyId, secretAccessKey, sessionToken);
 
-        LOGGER.info("Initializing S3Client accessKeyId {} - secretAccessKey {}", accessKeyId, secretAccessKey);
+        AwsSessionCredentials credentials = AwsSessionCredentials.create(
+                accessKeyId,
+                secretAccessKey,
+                sessionToken
+        );
 
         return S3Client.builder()
                 .region(Region.US_EAST_1)
                 .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        accessKeyId,
-                                        secretAccessKey
-                                )
-                        )
-                )
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
